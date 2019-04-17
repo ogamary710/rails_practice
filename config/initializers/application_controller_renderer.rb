d5c5538141e372b4a73595aa34f319a1d3d68f0a
ogamary710 <mariko.o@infratop.jp>
2019-04-17 06:58:46 +0000

    CRUD復習

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '8c3b43a2db2a0bf366a9fd16ecb425a2a243fe2233f488f786b1d7d932cb8e199ef4ac7b51bb0e3b189b6dee4a9961d3dc96878a5bd336f01de62b84022c3104'
